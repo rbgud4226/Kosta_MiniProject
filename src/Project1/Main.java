7ff7ae0ee03c11877e00a34b74e4a0626fa5c100
kyuhyung3 <kyuhyung3@naver.com>
2024-04-30 10:14:49 +0900

    "테스트용"

---
+++ src/Project1/Main.java
@@ -0,0 +1,15 @@
+package Project1;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Menu m = new Menu();
+		m.run(sc);
+		
+	}
+
+}
